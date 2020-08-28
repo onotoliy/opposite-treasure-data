9e671340a0b57d94d69895b02492a537cad971c2
Anatoliy Pokhresnyi <a.k.pohresniy@gmail.com>
2020-08-29 00:53:33 +0800

    add user dto

---
+++ src/commonMain/kotlin/com/github/onotoliy/opposite/data/User.kt
@@ -0,0 +1,16 @@
+package com.github.onotoliy.opposite.data
+
+import com.github.onotoliy.opposite.data.core.HasName
+import com.github.onotoliy.opposite.data.core.HasUUID
+import kotlinx.serialization.Serializable
+
+@Serializable
+class User(
+    override val uuid: String = "",
+    override val name: String = "",
+    val login: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val notifyByPhone: Boolean = false,
+    val notifyByEmail: Boolean = true
+) : HasUUID, HasName
